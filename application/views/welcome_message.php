218984a4487fa13e92d79c9ebea52f97c42c71c8
kakik09 <judeleon44@gmail.com>
2017-01-08 15:15:26 +0800

    add home

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -8,6 +8,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
   <!-- Compiled and minified JavaScript -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
+<style>
+  .fixed-bg {
+    background-image: url("images/maxresdefault.jpg");
+    min-height: 250px;
+     background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+
+}
+</style>
+<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
 </head>
 <body>
 	  <nav>
@@ -23,6 +35,165 @@ defined('BASEPATH') OR exit('No direct script access allowed');
       </ul>
     </div>
   </nav>
+
+  <div class="fixed-bg"></div>
+
+	<div 
+		style="
+			text-align: center; 
+			font-family: Poppins; 
+			background-color: #000000;
+			color: white;"
+	>
+		<h3 
+			style="
+				margin-top: 0px; 
+				padding-top: 20px;
+				font-size: 20px;"
+		>
+
+		Does the Philippines have a
+
+		</h3>
+
+		<h1 
+			style="
+				margin-bottom: 0px; 
+				padding-bottom: 20px;
+				font-size: 50px;"
+		>
+
+		DRUG PROBLEM?
+
+		</h1>
+
+	</div>
+
+	<div class="fixed-bg"></div>
+
+	<div 
+		style="
+			text-align: left; 
+			font-family: Poppins; 
+			background-color: #000000;
+			color: white;"
+	>
+		<h3 
+			style="
+				margin-top: 0px; 
+				padding-top: 20px;
+				padding-left: 20%;"
+
+		>
+
+		There were a total of
+
+		</h3>
+
+		<h1 
+			style="
+				margin-bottom: 0px; 
+				padding-left: 20%;
+				font-size: 50px;"
+		>
+
+		5,402 drug users admitted
+
+		</h1>
+
+		<h3 
+			style="
+				margin-bottom: 0px; 
+				padding-bottom: 20px;
+				padding-left: 20%;"
+		>
+
+		to rehabilitation facilities in 2015 alone.
+
+		</h3>
+
+	</div>
+
+
+	<div class="fixed-bg"></div>
+
+	<div 
+		style="
+			text-align: right; 
+			font-family: Poppins; 
+			background-color: #000000;
+			color: white;"
+	>
+
+	<h3 
+			style="
+				margin-top: 0px; 
+				padding-top: 20px;
+				padding-right: 20%;"
+
+		>
+
+		Among the substances abused were
+
+		</h3>
+
+		<h1 
+			style="
+				margin-bottom: 0px; 
+				padding-right: 20%;
+				font-size: 50px;"
+		>
+
+		Shabu, Marijuana, and Cocaine
+
+		</h1>
+
+		<h3 
+			style="
+				margin-bottom: 0px; 
+				padding-bottom: 20px;
+				padding-right: 20%;"
+		>
+
+		 
+
+		</h3>
+
+		
+	</div>
+
+	<div class="fixed-bg"></div>
+
+	<div 
+		style="
+			text-align: center; 
+			font-family: Poppins; 
+			background-color: #000000;
+			color: white;"
+	>
+		<h3 
+			style="
+				margin-top: 0px; 
+				padding-top: 20px;"
+		>
+
+		Learn more about drug abuse
+
+		</h3>
+
+		<h1 
+			style="
+				margin-bottom: 0px; 
+				padding-bottom: 20px;
+				font-size: 50px;"
+		>
+
+		NOW
+
+		</h1>
+
+	</div>
+
 
 </body>
 </html>

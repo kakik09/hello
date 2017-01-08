2f75037bc7ff339175cb4d817b24d1f8576b93c9
kakik09 <judeleon44@gmail.com>
2017-01-08 14:55:02 +0800

    add code igniter

---
+++ application/views/welcome_message.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
+
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
+</head>
+<body>
+	  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">DRUG NOTICE</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Home</a></li>
+        <li><a href="badges.html">News</a></li>
+        <li><a href="collapsible.html">Statistics</a></li>
+        <li><a href="">Substances</a></li>
+        <li><a href="">Directories</a></li>
+        <li><a href="">About us</a></li>
+      </ul>
+    </div>
+  </nav>
+
+</body>
+</html>
